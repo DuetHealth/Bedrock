40d801e35f37b160be978aac9622e0ed94d0aa0d
Ryan Wachowski <ryan.wachowski@duethealth.com>
2018-02-02 10:33:58 -0500

    Initial commit

---
+++ Bedrock/Bedrock.h
@@ -0,0 +1,19 @@
+//
+//  Bedrock.h
+//  Bedrock
+//
+//  Created by Ryan Wachowski on 2/2/18.
+//  Copyright © 2018 Duet Health LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bedrock.
+FOUNDATION_EXPORT double BedrockVersionNumber;
+
+//! Project version string for Bedrock.
+FOUNDATION_EXPORT const unsigned char BedrockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bedrock/PublicHeader.h>
+
+
