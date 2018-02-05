485f0daae008306e81ae5d9778d147e8e2d255b8
Ryan Wachowski <ryan.wachowski@duethealth.com>
2018-02-05 15:20:15 -0500

    Moves Bundle extension into Duet namespace.

--- Bedrock/Sources/Bundle.swift
+++ Bedrock/Sources/Bundle.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public extension Bundle {
+public extension DuetNamespace where Base: Bundle {
 
     /// Deserializes a JSON object. Will crash if file doesn't exist, is not a JSON object, is
     /// malformed, or is in a different bundle.
@@ -9,9 +9,17 @@ public extension Bundle {
     ///   - fileName: The name of the file to be loaded
     /// - Returns: A swift dictionary from the JSON in the file
     public func deserializeJSON(fileName: String) -> [String: Any] {
-        let url = self.url(forResource: fileName, withExtension: "json")!
+        let url = base.url(forResource: fileName, withExtension: "json")!
         let data = try! Data(contentsOf: url)
         return try! JSONSerialization.jsonObject(with: data) as! [String: Any]
+    }
+
+}
+
+public extension Bundle {
+
+    public var duet: DuetNamespace<Bundle> {
+        return DuetNamespace(base: self)
     }
 
 }
