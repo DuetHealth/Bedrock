d5c56c23e15d5e47c6415230276da6440a148fb7
Ryan Wachowski <ryan.wachowski@duethealth.com>
2018-02-08 14:21:45 -0500

    Changes Range extension to work with most common ranges, renames Optional extensions to eliminate ambiguity.

---
+++ Bedrock/Sources/Range+Clamp.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public extension CountableClosedRange {
+
+    /// Clamps values to this range.
+    ///
+    /// - Parameter value: the value to clamp.
+    /// - Returns: a value greater than or equal to `lowerBound` and less than or equal to `upperBound`.
+    public func clamp(_ value: Bound) -> Bound {
+        if value < self.lowerBound { return self.lowerBound }
+        if value > self.upperBound { return self.upperBound }
+
+        return value
+    }
+
+}
+
+public extension CountableRange {
+
+    /// Clamps values to this range.
+    ///
+    /// - Parameter value: the value to clamp.
+    /// - Returns: a value greater than or equal to `lowerBound` and less than `upperBound`.
+    public func clamp(_ value: Bound) -> Bound {
+        if value < self.lowerBound { return self.lowerBound }
+        if value > self.upperBound { return self.upperBound }
+
+        return value
+    }
+
+}
