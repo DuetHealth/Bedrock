b694a904b34905f32bc3b42c171f6fc0ad92b14f
Sylvester Watts <sylvester.watts@duethealth.com>
2018-02-08 10:42:12 -0500

    initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Bedrock",
+    products: [
+        .library(
+            name: "Bedrock",
+            targets: ["Bedrock"]),
+        ],
+    targets: [
+
+        .target(
+            name: "Bedrock",
+            dependencies: [],path:"Bedrock"),
+        .testTarget(
+            name: "BedrockTests",
+            dependencies: ["Bedrock"],path:"BedrockTests"),
+        ]
+)
+
+
+
